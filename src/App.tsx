1f534243ea9bc4ddd8fbf8677d4e1596de0a61b3
gomunkool <pivzz19881988@gmil.com>
2022-11-17 00:59:05 +0300

    feat: add SingInPage and Mui page

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from '@mui/material'
+import SignIn from './pages/signIn/SignIn'
+
+function App() {
+  return (
+    <div className="App">
+      <SignIn />
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/signIn/SignIn.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { SingInTitle } from './SignIn.styled'
+
+function SignIn() {
+  return <SingInTitle>SingIn</SingInTitle>
+}
+
+export default SignIn
