00d90940d3bec6295ac71df949b6008d732e9613
gomunkool <pivzz19881988@gmil.com>
2022-11-18 19:48:03 +0300

    feat: add wrapper

--- src/pages/signIn/SignIn.styled.ts
+++ src/pages/signIn/SignIn.styled.ts
@@ -2,6 +2,29 @@ import { Box } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
 export const SingInTitle = styled(Box)(() => ({
+display:'flex',
+flex: '0 0 50%',
+justifyContent: 'space-between'
+}));
+
+export const LeftPart = styled(Box)(() => ({
+backgroundColor: 'green',
+flex: '0 0 50%',
+height: '100vh',
+display:'flex',
+alignItems: 'center',
+justifyContent: 'center',
+ul:{
   color: 'red'
+}
+}));
+
+export const RightPart = styled(Box)(() => ({
+  backgroundColor: 'blue',
+  flex: '0 0 50%',
+  height: '100vh',
+  display:'flex',
+  alignItems: 'center',
+  justifyContent: 'center'
 }));
 
--- src/pages/signIn/SignIn.tsx
+++ src/pages/signIn/SignIn.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
-import { SingInTitle } from './SignIn.styled'
+import { Typography } from '@mui/material'
+
+import { SingInTitle, LeftPart, RightPart } from './SignIn.styled'
 
 function SignIn() {
-  return <SingInTitle>SingIn</SingInTitle>
+  return (
+    <SingInTitle>
+      <LeftPart>
+        <ul>
+          <li>
+            <Typography>Read what interests you.</Typography>
+          </li>
+          <li>Find out what the world is talking about.</li>
+          <li>Join the conversation.</li>
+        </ul>
+      </LeftPart>
+      <RightPart>eee</RightPart>
+    </SingInTitle>
+  )
 }
 
 export default SignIn
