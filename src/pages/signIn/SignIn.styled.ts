bfb6dd7ae045c5f0eaf9ae37ad3044bb86172c61
gomunkool <pivzz19881988@gmil.com>
2022-11-19 18:46:00 +0300

    feat: left part SingIn page

--- src/pages/signIn/SignIn.styled.ts
+++ src/pages/signIn/SignIn.styled.ts
@@ -8,19 +8,28 @@ justifyContent: 'space-between'
 }));
 
 export const LeftPart = styled(Box)(() => ({
-backgroundColor: 'green',
+backgroundColor: '#00acee',
 flex: '0 0 50%',
 height: '100vh',
 display:'flex',
 alignItems: 'center',
 justifyContent: 'center',
 ul:{
-  color: 'red'
+  listStyleType: 'none',
+  color: '#fff',
+li:{
+  display:'flex',
+  margin:'0 15px 15px 0',
+  svg:{
+    marginRight: '15px'
+  }
+}
+
 }
 }));
 
 export const RightPart = styled(Box)(() => ({
-  backgroundColor: 'blue',
+  backgroundColor: '#0080E6',
   flex: '0 0 50%',
   height: '100vh',
   display:'flex',
--- src/pages/signIn/SignIn.tsx
+++ src/pages/signIn/SignIn.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { Typography } from '@mui/material'
-
 import { SingInTitle, LeftPart, RightPart } from './SignIn.styled'
+import TwitterIcon from '@mui/icons-material/Twitter'
+import SearchIcon from '@mui/icons-material/Search'
+import PeopleIcon from '@mui/icons-material/People'
+import ForumIcon from '@mui/icons-material/Forum'
 
 function SignIn() {
   return (
@@ -9,13 +12,22 @@ function SignIn() {
       <LeftPart>
         <ul>
           <li>
+            <SearchIcon />
             <Typography>Read what interests you.</Typography>
           </li>
-          <li>Find out what the world is talking about.</li>
-          <li>Join the conversation.</li>
+          <li>
+            <PeopleIcon />
+            <Typography>Find out what the world is talking about.</Typography>
+          </li>
+          <li>
+            <ForumIcon />
+            <Typography>Join the conversation.</Typography>
+          </li>
         </ul>
       </LeftPart>
-      <RightPart>eee</RightPart>
+      <RightPart>
+        <TwitterIcon />
+      </RightPart>
     </SingInTitle>
   )
 }
